1719909e4b16eefece9b954aa82c5796a00d5a93
asinma2ch <43317719+asinma2ch@users.noreply.github.com>
2019-02-13 13:25:24 +0700

    implemented interaction

--- Assets/Scripts/TrenchBoyController.cs
+++ Assets/Scripts/TrenchBoyController.cs
@@ -11,17 +11,17 @@ public class TrenchBoyController : MonoBehaviour {
     public bool isCarrying = false;
     public float movementSpeed = 0.0f;
     public float maxSpeed = 0.0f;
+    public float interaction_time = 0.8f;
 
     Transform carry;
-    Meds med_crate;
-    Ammo ammo_crate;
     Rigidbody rb;
+    //Crate crate;
     Vector3 refVector = Vector3.zero;
-	// Use this for initialization
-	void Start () {
+    float delta = 0;
+    // Use this for initialization
+    void Start () {
         rb = GetComponent<Rigidbody>();
-        med_crate = GetComponent<Meds>();
-        ammo_crate = GetComponent<Ammo>();
+    //    crate = GetComponent<Crate>();
 	}
 	
 	// Update is called once per frame
@@ -34,6 +34,7 @@ public class TrenchBoyController : MonoBehaviour {
         interactions();
     }
 
+    // ↑↓→← WASD
     private void move()
     {
         if(isMovable == true)
@@ -41,7 +42,8 @@ public class TrenchBoyController : MonoBehaviour {
             if (isCarrying == true)
             {
                 maxSpeed *= 80.0f / 100.0f;
-            }            
+            }       
+            // ↑↓→←
             // LEFT
             if (Input.GetKey(KeyCode.LeftArrow))
             {
@@ -61,36 +63,62 @@ public class TrenchBoyController : MonoBehaviour {
             if (Input.GetKey(KeyCode.DownArrow))
             {
                 rb.velocity = Vector3.SmoothDamp(rb.velocity, Vector3.back * movementSpeed, ref refVector, 0.05f, maxSpeed);
-            }            
+            }
+            //========================
+            // WASD
+            if (Input.GetKey(KeyCode.A))
+            {
+                rb.velocity = Vector3.SmoothDamp(rb.velocity, Vector3.left * movementSpeed, ref refVector, 0.05f, maxSpeed);
+            }
+            // RIGHT
+            if (Input.GetKey(KeyCode.D))
+            {
+                rb.velocity = Vector3.SmoothDamp(rb.velocity, Vector3.right * movementSpeed, ref refVector, 0.05f, maxSpeed);
+            }
+            // UP
+            if (Input.GetKey(KeyCode.W))
+            {
+                rb.velocity = Vector3.SmoothDamp(rb.velocity, Vector3.forward * movementSpeed, ref refVector, 0.05f, maxSpeed);
+            }
+            // DOWN
+            if (Input.GetKey(KeyCode.S))
+            {
+                rb.velocity = Vector3.SmoothDamp(rb.velocity, Vector3.back * movementSpeed, ref refVector, 0.05f, maxSpeed);
+            }
         }       
     }
     
     private void interactions()
     {
-        if (isCarrying == false && Input.GetKeyDown(KeyCode.Space))
+        Debug.Log("khkj");
+        if (isCarrying == false && Input.GetKey(KeyCode.Space))
         {
             isMovable = false;
-            float holdStartTime = Time.time;
-            if (Input.GetKeyUp(KeyCode.Space))
-            {
-                float delta = Time.time - holdStartTime;
-                if(delta < 0.5f)
-                {
-                    // pick ammo/med pouch
-                    ammo_crate.take_pouch();
-                    Debug.Log("Picking up ammo/med pouch");
-                }
-                else if(delta >= 0.5f)
-                {
-                    // pick up the box itself
-                    Debug.Log("Picking up ammo/med crate");
-                    ammo_crate.move();
-                }
-            }
+            delta += Time.deltaTime;
+            //float holdStartTime = Time.time;
+
             isMovable = true;
-        }else if(isCarrying == true && Input.GetKey(KeyCode.Space)){
-            //throw or give item
         }
+        else
+        if ((isCarrying == false && Input.GetKeyUp(KeyCode.Space)))
+        {
+            //float delta = Time.time - holdStartTime;
+            if (delta < interaction_time)
+            {
+                // pick ammo/med pouch                    
+                Debug.Log("Picking up ammo/med pouch");
+            }
+            else if (delta >= interaction_time)
+            {
+                // pick up the box itself
+                Debug.Log("Picking up ammo/med crate");
+            }
+            delta = 0;
+        }
+        else if (isCarrying == true && Input.GetKey(KeyCode.Space))
+        {
+            //throw or give item
+        }        
     }  
     
 }
