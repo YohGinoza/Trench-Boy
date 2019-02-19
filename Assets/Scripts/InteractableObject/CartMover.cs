3ac5af6e4e38145254f4a3e2107eed96d69ff0f0
RatchanonPoom <ratchanon_p@sci.tu.ac.th>
2019-02-19 20:09:01 +0700

    Bug fixed: Player can't pick up/put the box down. Bug fixed: Cart move when player is not on pushing position. fixed: Texture missing on ally' request.

--- Assets/Scripts/InteractableObject/CartMover.cs
+++ Assets/Scripts/InteractableObject/CartMover.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CartControl : MonoBehaviour
+public class CartMover : MonoBehaviour
 {
     [SerializeField] private float MaxSpeed;
     [SerializeField] private float TimeToReachMaxSpeed;
@@ -16,32 +16,36 @@ public class CartControl : MonoBehaviour
     private float RailDistance;
     private Vector3 RailDirection;
 
-    private Vector3 refVelocity = Vector3.zero;
+    public bool isPushed = false;
 
-    bool isMoving = false;
+    private Vector3 refVelocity = Vector3.zero;
 
     private void Awake()
     {
-        this.transform.SetParent(Rail);
-        this.transform.localPosition = StartingEnd.localPosition + (Vector3.up * 0.5f);
+        this.transform.parent.SetParent(Rail);
+        this.transform.parent.localPosition = StartingEnd.localPosition + (Vector3.up * 0.5f);
 
         RailDirection = EndingEnd.position - StartingEnd.position;
         RailDistance = (EndingEnd.position - StartingEnd.position).magnitude;
 
-        rigidbody = this.GetComponent<Rigidbody>();
+        rigidbody = this.GetComponentInParent<Rigidbody>();
     }
 
     private void FixedUpdate()
     {
-        //if player not pushing and still on track slow down until speed reaches 0
-        if ((Vector3.Angle(rigidbody.velocity, RailDirection) < 90 && Vector3.Angle(rigidbody.velocity, EndingEnd.position - this.transform.position) < 90) || (Vector3.Angle(rigidbody.velocity, RailDirection) >= 90 && Vector3.Angle(rigidbody.velocity, StartingEnd.position - this.transform.position) < 90))
+        //if player not pushing
+        if (!isPushed)
         {
-            rigidbody.velocity = Vector3.SmoothDamp(rigidbody.velocity, Vector3.zero, ref refVelocity, TimeToCompletelyStop);
-        }
-        else //out of track, stop immediatly
-        {
-            rigidbody.velocity = Vector3.zero;
-            rigidbody.isKinematic = true;
+            //still on track slow down until speed reaches 0
+            if ((Vector3.Angle(rigidbody.velocity, RailDirection) < 90 && Vector3.Angle(rigidbody.velocity, EndingEnd.position - this.transform.position) < 90) || (Vector3.Angle(rigidbody.velocity, RailDirection) >= 90 && Vector3.Angle(rigidbody.velocity, StartingEnd.position - this.transform.position) < 90))
+            {
+                rigidbody.velocity = Vector3.SmoothDamp(rigidbody.velocity, Vector3.zero, ref refVelocity, TimeToCompletelyStop);
+            }
+            else //out of track, stop immediatly
+            {
+                rigidbody.velocity = Vector3.zero;
+                rigidbody.isKinematic = true;
+            }
         }
     }
 
@@ -49,6 +53,8 @@ public class CartControl : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            isPushed = true;
+
             if (Vector3.Angle((this.transform.position - other.transform.position), RailDirection) < 90)
             {
                 //move to ending end
@@ -61,18 +67,16 @@ public class CartControl : MonoBehaviour
             }
         }
     }
-    private void OntriggerExit(Collider other)
+
+    private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            isMoving = false;
-        }
+        isPushed = false;
     }
 
     private void MoveCart(Vector3 movingDirection)
     {
         //if cart is on track accelerate until reaches max speed
-        if ((Vector3.Angle(movingDirection, RailDirection) < 90 && Vector3.Angle(movingDirection, EndingEnd.position - this.transform.position) < 90)|| (Vector3.Angle(movingDirection, RailDirection) >= 90 && Vector3.Angle(movingDirection, StartingEnd.position - this.transform.position) < 90))
+        if ((Vector3.Angle(movingDirection, RailDirection) < 90 && Vector3.Angle(movingDirection, EndingEnd.position - this.transform.position) < 90) || (Vector3.Angle(movingDirection, RailDirection) >= 90 && Vector3.Angle(movingDirection, StartingEnd.position - this.transform.position) < 90))
         {
             rigidbody.isKinematic = false;
             rigidbody.velocity = Vector3.SmoothDamp(rigidbody.velocity, movingDirection.normalized * MaxSpeed, ref refVelocity, TimeToReachMaxSpeed);
