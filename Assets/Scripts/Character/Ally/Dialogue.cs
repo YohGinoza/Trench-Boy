dd178bdd6f9eb687c9648c8bbec97b84fd6bfc94
asinma2ch <43317719+asinma2ch@users.noreply.github.com>
2019-02-24 14:28:14 +0700

    Added new scene(Night phase) Added Prefab - Pvt. Ellis Anderson.

---
+++ Assets/Scripts/Character/Ally/Dialogue.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour
+{
+    TrenchBoyController player;    
+    public Text speech;
+    public int totalConversations = 0;
+    private int i = 0;
+    void Update()
+    {
+        player = GetComponent<TrenchBoyController>();
+        converse();
+    }
+
+    [TextArea(3,10)]
+    public string[] dialogue;
+
+    public void converse()
+    {
+        speech.enabled = true;
+        speech.text = dialogue[i];
+        speech.enabled = false;
+        if (dialogue[i + 1] != null)
+        {
+            i++;
+        }
+        StopAllCoroutines();
+        StartCoroutine(typeSentence(dialogue[i]));
+    }
+
+    IEnumerator typeSentence(string dialogue)
+    {
+        speech.text = "";
+        foreach(char letter in dialogue.ToCharArray())
+        {
+            speech.text += letter;
+            yield return null;
+        }
+    }
+}
