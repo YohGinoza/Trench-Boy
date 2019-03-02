6062237cc09deb0bea290435b994231a8ec44050
iypht46 <eiyaphat_s@sci.tu.ac.th>
2019-03-02 16:28:21 +0700

    Added: InventoryBar UI prefab and Wall_Cylinder prefab

---
+++ Assets/Scripts/UI/InventoryBar.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class InventoryBar : MonoBehaviour
+{   // Start is called before the first frame update
+
+    private Image[] med = new Image[3];
+    private Image[] ammo = new Image[3];
+    private Image[] HL = new Image[3];
+
+    private int SelectedItem;
+
+    private ItemType[] ItemInventory = new ItemType[2];
+
+    void Start()
+    {
+        //med1 = GetComponentInChildren<Cell_1_Med>().GetComponent<Image>();
+        med[0] = GameObject.Find("Cell_1_Med").GetComponent<Image>();
+        med[1] = GameObject.Find("Cell_2_Med").GetComponent<Image>();
+        med[2] = GameObject.Find("Cell_3_Med").GetComponent<Image>();
+
+        ammo[0] = GameObject.Find("Cell_1_Ammo").GetComponent<Image>();
+        ammo[1] = GameObject.Find("Cell_2_Ammo").GetComponent<Image>();
+        ammo[2] = GameObject.Find("Cell_3_Ammo").GetComponent<Image>();
+
+        HL[0] = GameObject.Find("Cell_1_HL").GetComponent<Image>();
+        HL[1] = GameObject.Find("Cell_2_HL").GetComponent<Image>();
+        HL[2] = GameObject.Find("Cell_3_HL").GetComponent<Image>();
+
+
+    }
+   // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < ItemInventory.Length; i++)
+        {
+            ItemInventory[i] = GameObject.Find("Player").GetComponent<InventorySystem>().ItemInventory[i];
+        }
+            
+        SelectedItem = GameObject.Find("Player").GetComponent<InventorySystem>().SelectedItem;
+
+        switch (SelectedItem)
+        {
+            case 0:
+                HL[0].fillAmount = 1;
+                HL[1].fillAmount = 0;
+                HL[2].fillAmount = 0;
+                break;
+            case 1:
+                HL[0].fillAmount = 0;
+                HL[1].fillAmount = 1;
+                HL[2].fillAmount = 0;
+                break;
+            case 2:
+                HL[0].fillAmount = 0;
+                HL[1].fillAmount = 0;
+                HL[2].fillAmount = 1;
+                break;
+        }
+
+        for(int i = 0; i < ItemInventory.Length; i++)
+        {
+            switch (ItemInventory[i]){
+                case ItemType.Ammo:
+                    ammo[i].fillAmount = 1;
+                    med[i].fillAmount = 0;
+                    break;
+                case ItemType.Med:
+                    ammo[i].fillAmount = 0;
+                    med[i].fillAmount = 1;
+                    break;
+                case ItemType.None:
+                    ammo[i].fillAmount = 0;
+                    med[i].fillAmount = 0;
+                    break;
+            }
+
+        }
+        
+
+    }
+}
