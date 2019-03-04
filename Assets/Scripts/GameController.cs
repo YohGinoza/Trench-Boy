1fe0c9b5ed9808fb39e8630e78dfc050d6fe3ad4
asinma2ch <43317719+asinma2ch@users.noreply.github.com>
2019-03-05 00:14:34 +0700

    Update GameController.cs
    added allies and days enum

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,6 +4,33 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    enum DAY_NO
+    {
+        DAY_1 = 0,
+        DAY_2,
+        DAY_3,
+        DAY_4,
+        DAY_5,
+        DAY_6,
+        DAY_7,
+    }
+
+    enum ALLIES
+    {
+        COL_WC = 0,
+        SGT_AB,
+        SGT_JC,
+        SGT_BS,
+        CPL_RG,
+        CPL_DR,
+        PVT_JR,
+        PVT_TB,
+        PVT_RW,
+        PVT_GS,
+        PVT_AS,
+        PVT_EA
+    }
+
     public int day = 0;
     public AllyTempBehaviour[] allies;
     public bool[] dead;
