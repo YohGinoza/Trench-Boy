59428ce05a4c00c39eda00e00f9f69dd1bc72b0e
asinma2ch <43317719+asinma2ch@users.noreply.github.com>
2019-03-09 19:26:32 +0700

    added ending txt and special txt arrays added conditions for ending txt and special txt arrays

--- Assets/Scripts/Character/Ally/Dialogue.cs
+++ Assets/Scripts/Character/Ally/Dialogue.cs
@@ -8,7 +8,13 @@ public class Dialogue : MonoBehaviour
     TrenchBoyController player;
     GameController gc;
     public Text speech;    
-    private int i = 0;
+    private int index_Friendship = 0;
+    private int index_Ending = 0;
+    private int index_Special = 0; // needs info from game controller
+
+    // once converse, set to true and trigger line Ex
+    public bool dayLimit = false;
+    public bool specialTrigger = false;
 
     private void Start()
     {
@@ -21,16 +27,57 @@ public class Dialogue : MonoBehaviour
     }
 
     [TextArea(3,10)]
-    public string[] dialogue;
-
+    public string[] dialogue_text;
+    public string[] ending_text;
+    public string[] special_text;
+    
+    // set dayLimit to true after conversing once
+    //+ reset dayLimit to true after every day has passed in GAMECONTROL script
     public void converse()
     {
-        speech.enabled = true;
-        speech.text = dialogue[i];        
-        if (dialogue[i + 1] != null)
-        {
-            i++;
+        if (!dayLimit)
+        {           
+            if (specialTrigger)
+            {
+                // if SPECIAL
+                //+ trigger this instead of NORMAL
+
+                // no more informational conversation
+                dayLimit = true;
+            }
+            else
+            {
+                // if NORMAL
+                speech.text = dialogue_text[index_Friendship];
+                if (dialogue_text[index_Friendship + 1] != null)
+                {
+                    index_Friendship++;
+                }
+
+                // no more informational conversation
+                dayLimit = true;
+            }
+            
+            
+            
         }
+        else
+        {
+            // ending lines
+            // goodbye quotes
+            speech.text = dialogue_text[index_Friendship];
+            if (dialogue_text[index_Ending] != null)
+            {
+                index_Ending++;
+            }
+        }
+        
+    }
+
+    // turn text box on/off
+    public void showText(bool show)
+    {
+        speech.enabled = show;
     }
 
 }
