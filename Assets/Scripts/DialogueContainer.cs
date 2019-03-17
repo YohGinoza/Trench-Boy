9d03911ac421a49a10fd5b68949546b17cefe3dd
asinma2ch <43317719+asinma2ch@users.noreply.github.com>
2019-03-17 18:05:26 +0700

    added dialogue database and dialoguecontainer script

---
+++ Assets/Scripts/DialogueContainer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using UnityEngine;
+
+[XmlRoot("DialogueCollection")]
+public class DialogueContainer
+{
+    [XmlAttribute("name")]
+    public string name;    
+
+
+    public static DialogueContainer Load(string path)
+    {
+
+    }
+   
+}
