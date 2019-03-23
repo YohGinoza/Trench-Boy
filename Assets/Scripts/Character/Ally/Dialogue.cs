6f8b2908e542abb16ad14c102e7005496d007896
asinma2ch <43317719+asinma2ch@users.noreply.github.com>
2019-03-23 17:48:06 +0700

    added resources folder redid all dialogue related script removed all alles prefab added sgt.allen prefab using new script

--- Assets/Scripts/Character/Ally/Dialogue.cs
+++ Assets/Scripts/Character/Ally/Dialogue.cs
@@ -3,94 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Xml;
+using System.Xml.Serialization;
 
-public class Dialogue : MonoBehaviour
-{
-    //public event Action talkEvent; //trigger everything under that event
+public class Dialogue
+{ 
+    [XmlAttribute("name")]
+    public string name;
 
-    [TextArea(3, 10)]
-    public string[] dialogue_text;
-    public string[] ending_text;
-    public string[] special_text;
+    [XmlElement("Who")]
+    public int who;
 
-    TrenchBoyController player;
-    GameController gc;
-    public Text speech;    
-    private int index_Friendship = 0;
-    private int index_Ending = 0;
-    private int index_Special = 0;    
-    // once converse, set to true and trigger line Ex
-    public bool dayLimit = false;
-    public bool specialTrigger = false;
-    private bool readyToClearText = false;
+    [XmlElement("Speaker")]
+    public string speaker;
 
-    [SerializeField] private Ally[] friends;
-    private int diedToday;
-    
-    bool[] dietoday; // ??????????????????????????????????????????????????????????? day_end script
+    [XmlElement("Type")]
+    public string type;
+
+    [XmlElement("ID")]
+    public int id;
+
+    [XmlElement("Line")]
+    public int line;
+
+    [XmlElement("Text")]
+    public string text;
+
     private void Start()
     {
-        gc = GetComponent<GameController>();     
-        
-        for(int i =0;i<friends.Length;i++)
-        {
-            if (dietoday[(int)friends[i]])
-            {
-                specialTrigger = true;
-                diedToday = i;
-                break;
-            }
-        }        
-    }
 
-    // set dayLimit to true after conversing once
-    //+ reset dayLimit to true after every day has passed in GAMECONTROL script
-    public void converse()
-    {
-        if (!speech.enabled)
-        {
-            speech.enabled = true;
-        }
-        if (!dayLimit)
-        {           
-            if (specialTrigger)
-            {
-                // if SPECIAL
-                //+ trigger this instead of NORMAL
-                speech.text = special_text[diedToday]; // don't forget to delete the text
-                // no more informational conversation
-                readyToClearText = true;
-                specialTrigger = false;
-                dayLimit = true;
-            }
-            else
-            {
-                // if NORMAL
-                speech.text = dialogue_text[index_Friendship];
-                if (dialogue_text[index_Friendship + 1] != null)
-                {
-                    index_Friendship++;
-                }                
-                // no more informational conversation
-                dayLimit = true;
-            }
-            gc.NightTimeInteractCounter++; // increment night limit counter
-        }
-        else
-        {
-            // ending lines
-            // goodbye quotes
-            speech.text = dialogue_text[index_Friendship];
-            if (dialogue_text[index_Ending + 1] != null)
-            {
-                index_Ending++;
-            }
-            else
-            {
-                speech.enabled = false;
-            }
-            
-        }
-        
+        Debug.Log("YAY");
     }
 }
---
+++ Assets/Scripts/Character/Ally/DialogueLoader.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueLoader : MonoBehaviour
+{
+    //public event Action talkEvent; //trigger everything under that event
+    [SerializeField] private Ally ThisPerson;
+
+    [TextArea(3, 10)]
+    public string[,] dialogue_text = new string[3,10];
+    public string[] ending_text;
+    public string[,] special_text;
+
+    public bool[,] D_NPCSpeaking = new bool[3,10];    
+    public bool[,] S_NPCSpeaking;
+
+    public int index_Friendship = 0;
+    public int index_Ending = 0;
+
+    public string[,] D = new string[7, 30];
+    
+
+    TrenchBoyController player;
+    GameController gc;
+    public Text speech;
+    private int lineCounter = 0;
+    // once converse, set to true and trigger line Ex
+    public bool dayLimit = false;
+    public bool specialTrigger = false;
+    private bool readyToClearText = false;
+
+    [SerializeField] private Ally[] friends;
+    private int diedToday;
+    
+    bool[] dietoday; // ??????????????????????????????????????????????????????????? day_end script
+    //======================================================
+
+    public const string path = "Test";  
+
+    private void Start()
+    {
+        gc = GetComponent<GameController>();
+
+        for (int i = 0; i < friends.Length; i++)
+        {
+            if (dietoday[(int)friends[i]])
+            {
+                specialTrigger = true;
+                diedToday = i;
+                break;
+            }
+        }
+
+        DialogueContainer dc = DialogueContainer.Load(path);
+
+        foreach(Dialogue dialogue in dc.dialogues)
+        {
+            if (dialogue.who == (int)ThisPerson)
+            {      
+                if(dialogue.type == "D")
+                {
+                    dialogue_text[dialogue.id, dialogue.line] = dialogue.text;
+                    D_NPCSpeaking[dialogue.id, dialogue.line] = dialogue.speaker == "NPC";
+                }
+                else if (dialogue.type == "E")
+                {
+                    ending_text[dialogue.id] = dialogue.text;
+                }
+                else if (dialogue.type == "S")
+                {
+                    special_text[dialogue.id, dialogue.line] = dialogue.text;
+                    S_NPCSpeaking[dialogue.id, dialogue.line] = dialogue.speaker == "NPC";
+                }
+                else
+                {
+                    print("UYA");
+                }
+            }
+        }
+
+        //========================================================
+    }
+
+    private void Update()
+    { 
+        
+    }
+
+    // set dayLimit to true after conversing once
+    //+ reset dayLimit to true after every day has passed in GAMECONTROL script
+    
+    public void converse()
+    {
+        if (!speech.enabled)
+        {
+            speech.enabled = true;
+        }
+        if (!dayLimit)
+        {
+            if (specialTrigger)
+            {
+                // if SPECIAL
+                //+ trigger this instead of NORMAL
+                speech.text = special_text[diedToday,lineCounter++]; // don't forget to delete the text
+                // no more informational conversation
+                readyToClearText = true;
+                if (special_text[diedToday, lineCounter] == null)
+                {
+                    
+                }
+                else
+                {
+                    specialTrigger = false;
+                    dayLimit = true;
+                    lineCounter = 0;
+                }
+            }
+            else
+            {
+                // if NORMAL
+                speech.text = dialogue_text[index_Friendship, lineCounter];
+                if (dialogue_text[index_Friendship, lineCounter] != null)
+                {
+                    index_Friendship++;
+                    lineCounter++;                    
+                }
+                else
+                {
+                    // no more informational conversation
+                    dayLimit = true;
+                    lineCounter = 1;
+                }
+            }
+            gc.NightTimeInteractCounter++; // increment night limit counter
+        }
+        else
+        {
+            // ending lines
+            // goodbye quotes
+            speech.text = ending_text[index_Ending];
+            if (ending_text[index_Ending + 1] != null)
+            {
+                index_Ending++;                
+            }
+            else
+            {
+                speech.enabled = false;
+            }
+        }
+    }
+    
+}
