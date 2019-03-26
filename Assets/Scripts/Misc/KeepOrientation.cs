8107f5ba17f6ddd576492dd498c3ea2b24a916ac
RatchanonPoom <ratchanon_p@sci.tu.ac.th>
2019-03-26 23:52:58 +0700

    Bug fixed: Player text is inverted when turning.

--- Assets/Scripts/Misc/KeepOrientation.cs
+++ Assets/Scripts/Misc/KeepOrientation.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class KeepOrientation : MonoBehaviour
 {
     public Vector3 OriginalEulerAngle;
+    public Vector3 OriginalScale;
+    public Vector3 OrginalChildPos;
+    Transform parent;
 
     private void Start()
     {
         OriginalEulerAngle = this.transform.eulerAngles;
+        OriginalScale = this.transform.lossyScale;
+        OrginalChildPos = this.transform.localPosition;
+        parent = this.transform.parent;
     }
 
     private void FixedUpdate()
     {
-        this.transform.rotation = Quaternion.Euler(OriginalEulerAngle);        
+        this.transform.rotation = Quaternion.Euler(OriginalEulerAngle);
+        this.transform.parent = null;
+        this.transform.localScale = OriginalScale;
+        this.transform.parent = parent;
+        this.transform.localPosition = OrginalChildPos;
     }
 }
