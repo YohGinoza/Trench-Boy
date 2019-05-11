e052cf1847e41600fcef51eb60648565acdd6a94
WolfePutaski <43819232+WolfePutaski@users.noreply.github.com>
2019-05-11 10:43:49 +0700

    - Commented on BloomController (the \\ thing) - Changed bullet gravity and mass. Check if the ally can hit the enemy becauseI tried and they aim too high

--- Assets/CinematicOnly/BloomController.cs
+++ Assets/CinematicOnly/BloomController.cs
@@ -1,19 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Rendering.PostProcessing;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.Rendering.PostProcessing;
 
-public class BloomController : MonoBehaviour
-{
-    [SerializeField] private Bloom bloom;
-    [SerializeField] private float IntensityStart;
-    [SerializeField] private float IntensityEnd;
-    [SerializeField] private float TransitionTime;
-    float timer = 0;
+//public class BloomController : MonoBehaviour
+//{
+//    [SerializeField] private Bloom bloom;
+//    [SerializeField] private float IntensityStart;
+//    [SerializeField] private float IntensityEnd;
+//    [SerializeField] private float TransitionTime;
+//    float timer = 0;
 
-    private void FixedUpdate()
-    {
-        timer += Time.fixedDeltaTime / TransitionTime;
-        bloom.intensity.value = Mathf.Lerp(IntensityStart, IntensityEnd, timer);
-    }
-}
+//    private void FixedUpdate()
+//    {
+//        timer += Time.fixedDeltaTime / TransitionTime;
+//        bloom.intensity.value = Mathf.Lerp(IntensityStart, IntensityEnd, timer);
+//    }
+//}
